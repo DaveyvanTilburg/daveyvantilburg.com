7a0f89a136a472144d2c25ff2198680e1f7e4f7e
Davey van Tilburg <daveyvantilburg@gmail.com>
2020-09-19 21:14:29 +0200

    encapsulate behaviour in proper models

--- DaveyVanTilburgWebsite/DaveyVanTilburgWebsite/Controllers/PostsController.cs
+++ DaveyVanTilburgWebsite/DaveyVanTilburgWebsite/Controllers/PostsController.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using System.Linq;
+﻿using DaveyVanTilburgWebsite.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DaveyVanTilburgWebsite.Controllers
@@ -7,23 +6,9 @@ namespace DaveyVanTilburgWebsite.Controllers
     public class PostsController : Controller
     {
         public IActionResult Index()
-        {
-            string[] posts = Directory.GetFiles("./Posts", "*.html")
-                .Select(Path.GetFileNameWithoutExtension).ToArray();
-
-            ViewBag.Posts = posts;
-
-            return View();
-        }
+            => View(new MyThoughtsList());
 
         public IActionResult PostView(string postName)
-        {
-            string postHtml = System.IO.File.ReadAllText($"./Posts/{postName}.html");
-
-            ViewBag.PostHtml = postHtml;
-            ViewBag.PostName = postName;
-
-            return View();
-        }
+            => View(new MyThoughtsPost(postName));
     }
 }
---
+++ DaveyVanTilburgWebsite/DaveyVanTilburgWebsite/Models/MyThoughtsList.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace DaveyVanTilburgWebsite.Models
+{
+    public class MyThoughtsList
+    {
+        private readonly string _path;
+        private readonly string _searchPattern;
+        public MyThoughtsList()
+        {
+            _path = "./Posts";
+            _searchPattern = "*.html";
+        }
+
+        public string[] PostNames()
+            => Directory.GetFiles(_path, _searchPattern)
+                .Select(Path.GetFileNameWithoutExtension).ToArray();
+    }
+}
