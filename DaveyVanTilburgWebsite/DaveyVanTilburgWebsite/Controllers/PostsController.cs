740ccb1ec308c0cabdaaec7985786374cd1fe866
Davey van Tilburg <daveyvantilburg@gmail.com>
2020-09-19 12:17:56 +0200

    Add page for posts with first post

---
+++ DaveyVanTilburgWebsite/DaveyVanTilburgWebsite/Controllers/PostsController.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DaveyVanTilburgWebsite.Controllers
+{
+    public class PostsController : Controller
+    {
+        public IActionResult Index()
+        {
+            string[] posts = Directory.GetFiles("./Posts", "*.html")
+                .Select(Path.GetFileNameWithoutExtension).ToArray();
+
+            ViewBag.Posts = posts;
+
+            return View();
+        }
+
+        public IActionResult PostView(string postName)
+        {
+            string postHtml = System.IO.File.ReadAllText($"./Posts/{postName}.html");
+
+            ViewBag.PostHtml = postHtml;
+            ViewBag.PostName = postName;
+
+            return View();
+        }
+    }
+}
