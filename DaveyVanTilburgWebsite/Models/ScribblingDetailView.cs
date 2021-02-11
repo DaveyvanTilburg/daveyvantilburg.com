a884856d136b334a6e4a661696ed5ab1fb8f5ea5
Davey van Tilburg <daveyvantilburg@gmail.com>
2021-02-11 13:13:22 +0100

    Add 'Virtue and moral compass' Add regex replace for html body so I don't have to manually remove it

--- DaveyVanTilburgWebsite/Models/ScribblingDetailView.cs
+++ DaveyVanTilburgWebsite/Models/ScribblingDetailView.cs
@@ -1,4 +1,6 @@
-﻿namespace DaveyVanTilburgWebsite.Models
+﻿using System.Text.RegularExpressions;
+
+namespace DaveyVanTilburgWebsite.Models
 {
     public class ScribblingDetailView
     {
@@ -10,6 +12,14 @@
 
         public string Title() => _fileName;
 
-        public string Html() => System.IO.File.ReadAllText($"./Scribblings/{_fileName}.html");
+        public string Html()
+        {
+            string fileContents = System.IO.File.ReadAllText($"./Scribblings/{_fileName}.html");
+
+            string strippedBodyStart = Regex.Replace(fileContents, "<!DOC.*<body.*?>", "", RegexOptions.Singleline);
+            string result = Regex.Replace(strippedBodyStart, "</body.*>", "", RegexOptions.Singleline);
+
+            return result;
+        }
     }
 }
